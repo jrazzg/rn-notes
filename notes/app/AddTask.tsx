474ac05fde52f4dea508c407a684915130243e2c
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-09-02 03:46:59 +0800

    Add textInput

---
+++ notes/app/AddTask.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, TouchableOpacity } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+const AddTaskScreen = () => {
+    const [task, setTask] = useState<string>('')
+
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView style={styles.container}>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setTask}
+                    value={task}
+                    placeholder="Insert note"
+                    returnKeyType="done"
+                />
+                <TouchableOpacity style={styles.addButton}>
+                    <Text style={styles.buttonText}>Submit</Text>
+                </TouchableOpacity>
+            </SafeAreaView>
+        </SafeAreaProvider>
+    )
+};
+
+export default AddTaskScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#ffffffff',
+        flex: 1,
+        padding: 20,
+    },
+    input: {
+        flex: 1,
+        textAlignVertical: 'top',
+        marginBottom: 20,
+        padding: 20,
+        backgroundColor: '#f0f0f0ff',
+        borderRadius: 10,
+    },
+    addButton: {
+        backgroundColor: '#004dc0ff',
+        borderRadius: 50,
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText: {
+        color: '#fff',
+    },
+});
--- notes/app/index.tsx
+++ notes/app/index.tsx
@@ -1,15 +1,39 @@
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const router = useRouter();
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
+
+      <Pressable style={styles.addButton} onPress={() => router.navigate('/AddTask')}>
+        <Text style={styles.addButtonText}>+</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ffffffff',
+    flex: 1,
+    padding: 20,
+  },
+  addButton: {
+    backgroundColor: '#004dc0ff',
+    width: 60,
+    aspectRatio: 1,
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    borderRadius: 50,
+    margin: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  addButtonText: {
+    color: '#fff',
+    fontSize: 23,
+  },
+});
